be6c0adc2fa4bdb3d960e04b71c13d212e19c62e
leerobertdyer <leerobertdyer@gmail.com>
2023-10-18 21:25:17 -0400

    first commit

---
+++ modules/myMod.py
@@ -0,0 +1,9 @@
+from math import pi
+
+
+def area(length, width):
+    return f'Your house is {length * width} square feet!'
+
+def circ(r):
+    return f'Your circle has a radius of {(2*pi) * r}!'
+
